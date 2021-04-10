29ae181ef82215ff9621fff97e6e536e05313ec1
Hoang NGUYEN <baauco@gmail.com>
2021-04-10 08:26:22 +0700

    Updated formatting

--- CO2018/Lab3/1952255/code.c
+++ CO2018/Lab3/1952255/code.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void *hello(void *tid)
 {
   printf("Hello from thread %d\n", *(int *)tid);
@@ -11,11 +13,14 @@ int main()
 {
   pthread_t tid[10];
   int i = 0;
+
   for (i = 0; i < 10; i++)
   {
-    pthread_create(&tid[i], NULL, hello, (void *)&i);
+    if (pthread_create(&tid[i], NULL, hello, (void *)&i) != 0)
+      return -1;
     pthread_join(tid[i], NULL);
   }
+
   pthread_exit(NULL);
 
   return 0;
