2a41469fea5825274442e928c4152ba858ef2b60
Hoang NGUYEN <baauco@gmail.com>
2021-04-26 00:00:28 +0700

    Updated lab

--- CO2039/OOP/Lab/car_rental.hpp
+++ CO2039/OOP/Lab/car_rental.hpp
@@ -4,9 +4,6 @@
  * Knowing the difference between the interface and implementation
  * Thinking more abstractly
  * Giving the user the minimal interface possible
- * EuropCar
- * Dollar Rent A Car
- * Enterprise Rent-A-Car
  */
 
 /*
@@ -15,75 +12,220 @@
  * Provide multiple constructors for creating a car rent. Define multiple methods that add a vehicle to the fleet of the said car rental company.
  */
 
+/*
+ * For the sake of fleet maintenance, the car rental company would like to keep track the routine maintenance performed on its vehicles. More specifically, each vehicle has a service history telling how and when its engine, transmission and tires get serviced. In your source code, define the following methods as virtual for your superclass Vehicle. Override them in your subclasses. When implementing them, you should check if the vehicle, depending on its current mileage, really needs to have its engine / transmission / tires serviced. Vehicles of different types get serviced differently at different frequencies.
+ *
+ * serviceEngine // add a record to the vehicle history telling how the engine of the vehicle gets serviced (oil change, minor, major)
+ *
+ * serviceTransmission // add a record to the vehicle history telling how the transmission of the vehicle gets serviced (fluid change, minor, overhaul)
+ *
+ * serviceTires // add a record to the vehicle history telling how the tires of the vehicle get serviced (adjustment, replacement)
+ *
+ * Make sure that Vehicle is an abstract class that is connected to ServiceHistory via a composition.
+ *
+ * Your class CarRentalManagement has a method, called serviceFleet, that basically iterates through all vehicles it manages. For each vehicle, this operation calls the above methods to perform appropriate services if needed.
+ */
+
+/*
+ *  BookAndRent is an interface that is implemented by CarRentalManagement
+ *
+ *  Should we have an interface that represents the job of getting the car fleet serviced?
+ *
+ *  Rent is a class that describes a car rental contract. Any interface being implemented by Rent?
+ */
+
+/*
+ * Remember that a vehicle in your car rental management company is associated with a service history, which keeps track of all maintenance jobs that have been done to the vehicle in question. A maintenance job should record the following pieces of information: mileage of the vehicle and date/time when the job was carried out, kind of service being performed (e.g., tires, engine oil, transmission fluid), costs, garage where the job was done.
+ *
+ * In C++, overload the subtraction - operator to imply the distance the vehicle had been driven between any two maintenance jobs. Moreover, you may overload the comparison operators to see if a maintenance job takes place before or after another.
+ *
+ * Note: don't forget to check if the given two maintenance jobs were done for the same vehicle. The above-mentioned operators would go meaningless otherwise.
+ */
+
+/*
+ * Create a document that captures a certain maintenance activity. Attempt to print the JSON document out to your console.
+ *
+ * Go further creating a JSON document that represents the whole service history of a vehicle.
+ *
+ * You are not expected to try parsing a JSON document you have created at this stage.
+*/
+
 #ifndef CAR_RENTAL_HPP
 #define CAR_RENTAL_HPP
 
 #include <string>
 #include <vector>
+#include <iostream>
 
-class Vehicle
+#pragma region Vehicles
+class IVehicle
 {
 public:
-  Vehicle();
-  Vehicle(int, int, int, std::string, std::string, std::string, std::string);
+  virtual ~IVehicle() {}
+  virtual int getWeight() const = 0;
+  virtual int getSeat() const = 0;
+  virtual int getYear() const = 0;
+  virtual std::string getType() const = 0;
+  virtual std::string getBrand() const = 0;
+  virtual std::string getModel() const = 0;
+  virtual std::string getPlate() const = 0;
+}; // end class IVehicle
+
+class Vehicle : public IVehicle
+{
+protected:
+  int weight, seats, year, mileage, cargo_space;
+  std::string type, // * Car, Truck, Van, SUV...
+      brand,        // * Toyota, Ford...
+      model,        // * Camry, Mustang...
+      plate,        // * License plate
+      drivetrain;   // * 2x2, 4x4, 2x6, 4x6, 6x6...
+  bool available = true;
 
 public:
-  int getWeight() const;
-  int getSeat() const;
-  int getYear() const;
-  std::string getType() const;
-  std::string getBrand() const;
-  std::string getModel() const;
-  std::string getPlate() const;
-  void setWeight(int);
-  void setSeat(int);
-  void setYear(int);
-  void setType(std::string);
-  void setBrand(std::string);
-  void setModel(std::string);
-  void setPlate(std::string);
-}; // class Vehicle
+  Vehicle(int weight, int seats, int cargo_space, int year, std::string type, std::string brand, std::string model, std::string plate, std::string drivetrain = "2x4") : weight(weight), seats(seats), cargo_space(cargo_space), year(year), type(type), brand(brand), model(model), plate(plate), drivetrain(drivetrain) {}
 
-class Contract
+public:
+  int getWeight() const { return weight; }
+  int getSeat() const { return seats; }
+  int getCargoSpace() const { return cargo_space; }
+  int getYear() const { return year; }
+  int getMileage() const { return mileage; }
+  std::string getType() const { return type; }
+  std::string getBrand() const { return brand; }
+  std::string getModel() const { return model; }
+  std::string getPlate() const { return plate; }
+  bool isAvailable() const { return available; }
+  void setWeight(int value) { weight = value; }
+  void setSeat(int value) { seats = value; }
+  void setType(std::string value) { type = value; }
+  void setAvailability(bool value) { available = value; }
+
+public:
+  virtual void serviceEngine();
+  virtual void serviceTransmission();
+  virtual void serviceTires();
+}; // end class Vehicle
+
+class Sedan : public Vehicle
 {
 public:
-  Contract();
-  Contract(int, int, int, std::string, std::string, std::string, Vehicle *);
+  Sedan(int weight, int seats, int cargo_vroom, int year, std::string brand, std::string model, std::string plate, std::string drivetrain = "2x4") : Vehicle{weight, seats, cargo_vroom, year, "Sedan", brand, model, plate, drivetrain} {}
+}; // end class Sedan
+
+class SUV : public Vehicle
+{
+public:
+  SUV(int weight, int seats, int cargo_vroom, int year, std::string brand, std::string model, std::string plate, std::string drivetrain = "2x4") : Vehicle{weight, seats, cargo_vroom, year, "SUV", brand, model, plate, drivetrain} {}
+}; // end class SUV
+
+class Coupe : public Vehicle
+{
+public:
+  Coupe(int weight, int seats, int cargo_vroom, int year, std::string brand, std::string model, std::string plate, std::string drivetrain = "2x4") : Vehicle{weight, seats, cargo_vroom, year, "Coupe", brand, model, plate, drivetrain} {}
+}; // end class Coupe
+
+class Van : public Vehicle
+{
+public:
+  Van(int weight, int seats, int cargo_vroom, int year, std::string brand, std::string model, std::string plate, std::string drivetrain = "2x4") : Vehicle{weight, seats, cargo_vroom, year, "Van", brand, model, plate, drivetrain} {}
+}; // end class Van
+
+class Truck : public Vehicle
+{
+public:
+  Truck(int weight, int seats, int cargo_vroom, int year, std::string brand, std::string model, std::string plate, std::string drivetrain = "2x4") : Vehicle{weight, seats, cargo_vroom, year, "Truck", brand, model, plate, drivetrain} {}
+
+}; // end class Truck
+#pragma endregion
+
+#pragma region Contract
+class IContract
+{
+public:
+  virtual ~IContract() {}
+  virtual void who() const = 0;
+  virtual void what() const = 0;
+}; // end class IContract
+
+class Contract : public IContract
+{
+protected:
+  std::string name, socialID, licenseID, dob;
+  bool male;
+  Vehicle *vehicle;
 
 public:
-  int getID() const;
-  int getTimeStart() const;
-  int getTimeEnd() const;
-  std::string getName() const;
-  std::string getLocStart() const;
-  std::string getLocEnd() const;
-  Vehicle *getVehicle() const;
-  void setID(int);
-  void setTimeStart(int);
-  void setTimeEnd(int);
-  void setName(std::string);
-  void setLocStart(std::string);
-  void setLocEnd(std::string);
-  void setVehicle(Vehicle *);
+  Contract(std::string name, std::string socialID, std::string licenseID, std::string dob, bool male, Vehicle *vehicle) : name(name), socialID(socialID), licenseID(licenseID), dob(dob), male(male), vehicle(vehicle) {}
+  Contract(std::string name, std::string socialID, std::string licenseID, std::string dob, Vehicle *vehicle) : name(name), socialID(socialID), licenseID(licenseID), dob(dob), male(true), vehicle(vehicle) {}
 
 public:
-  Vehicle *operator*();
-}; // class Contract
+  void who() const
+  {
+    std::cout << "Name: " << name << std::endl
+              << "Social ID: " << socialID << std::endl
+              << "License ID: " << licenseID << std::endl
+              << "Date of birth: " << dob << "Gender: " << ((male) ? "Male" : "Female") << std::endl;
+  }
 
+  void what() const
+  {
+    std::cout << vehicle->getYear() << " " << vehicle->getBrand() << " " << vehicle->getModel() << " " << vehicle->getType() << std::endl;
+  }
+  Vehicle *operator*() { return vehicle; }
+}; // end class Contract
+#pragma endregion
+
+#pragma region Rental Service
 class IBookAndRent
 {
 public:
-  virtual int netRent() = 0;
-  virtual int newBook() = 0;
-  virtual int getRent() = 0;
-  virtual int getBook() = 0;
+  virtual ~IBookAndRent() {}
+  virtual int newRent(std::string name, std::string socialID, std::string licenseID, std::string dob, bool male, Vehicle *vehicle) = 0;
+  virtual int newBook(std::string name, std::string socialID, std::string licenseID, std::string dob, bool male, Vehicle *vehicle) = 0;
+  virtual int endRent() = 0;
+  virtual int endBook() = 0;
 }; // class IBookAndRent
 
 class BookAndRent : public IBookAndRent
 {
+protected:
+  std::vector<Contract *> bookings;
+  std::vector<Contract *> rentals;
+
 public:
-  int newRent(Contract *);
-  int newBook(Contract *);
+  ~BookAndRent() { declare_bankruptcy(); }
+  int newRent(std::string name, std::string socialID, std::string licenseID, std::string dob, bool male, Vehicle *vehicle)
+  {
+    vehicle->setAvailability(false);
+    rentals.push_back(new Contract(name, socialID, licenseID, dob, male, vehicle));
+    return 0;
+  }
+  int newBook(std::string name, std::string socialID, std::string licenseID, std::string dob, bool male, Vehicle *vehicle)
+  {
+    vehicle->setAvailability(false);
+    bookings.push_back(new Contract(name, socialID, licenseID, dob, male, vehicle));
+    return 0;
+  }
+  int endRent()
+  {
+    std::cout << "Thank you for using our service." << std::endl;
+    return 0;
+  }
+  int endBook()
+  {
+    std::cout << "Thank you for using our service." << std::endl;
+    return 0;
+  }
+  void declare_bankruptcy()
+  {
+    std::cout << "Thank you for being a part with us through all those years." << std::endl
+              << "All good things must come to an end, and this is the end of our journey together." << std::endl
+              << "We hope that we have created great memories for you to cherish." << std::endl
+              << "RIP 2021 - 2021" << std::endl;
+  }
+  void serviceFleet();
 }; // class BookAndRent
+#pragma endregion
 
 #endif
--- CO2039/OOP/Lab/main.cpp
+++ CO2039/OOP/Lab/main.cpp
@@ -1,7 +1,159 @@
 #include "car_rental.hpp"
 #include <iostream>
+#include <string>
+#include <string.h> // for strcmp()
+#include <unistd.h> // for system()
 
 int main(int argc, char **argv)
 {
+  char choice;
+  bool running = true;
+  std::string name, socialID, licenseID, dob;
+  bool male;
+  IBookAndRent *BnR = new BookAndRent();
+
+  std::string foo;
+  int opt = 0;
+
+#pragma region Vehicles
+  std::vector<Vehicle *> vehicles;
+  vehicles.push_back(new Sedan(550, 4, 450, 2010, "Toyota", "Corolla", "IDGAF"));
+  vehicles.push_back(new SUV(1080, 7, 900, 2016, "Toyota", "Rush", "DEADBEEF"));
+  vehicles.push_back(new Coupe(990, 4, 400, 2018, "Lexus", "LC 500", "STAT200", "4x4"));
+  vehicles.push_back(new Sedan(1200, 4, 340, 1994, "Ford", "Mustang", "THUG"));
+  vehicles.push_back(new Van(2400, 16, 1000, 2012, "Toyota", "Hiace", "HITHERE"));
+  vehicles.push_back(new Truck(3500, 2, 1200, 2019, "Ford", "F150", "MISBEHAVE"));
+  vehicles.push_back(new Truck(10500, 4, 10000, 1984, "Tatra", "IDK", "DMZ", "8x8"));
+  vehicles.push_back(new Vehicle(20000, 3, 18000, 2008, "Tanker", "MAC", "GAZ", "UCME", "4x8"));
+  vehicles.push_back(new Sedan(1600, 4, 1100, 2018, "Tesla", "Model 3", "BACON"));
+  vehicles.push_back(new Sedan(550, 4, 400, 1981, "Reliant Robin", "Mk3", "ROLLER", "2x3"));
+#pragma endregion
+
+  while (running)
+  {
+    std::cout << "Hey there handsome." << std::endl
+              << "Since you're here, do you want to" << std::endl
+              << "[0] Exit the program and kill yourself" << std::endl
+              << "[1] Rent a car" << std::endl
+              << "[2] Book a car" << std::endl
+              << "[3] Show all available vehicles" << std::endl;
+    std::cin >> choice;
+
+    switch (choice)
+    {
+    case '0':
+      running = false;
+      system("clear");
+      break;
+
+    case '1':
+      std::cout << "Gotcha. Getting you there right now." << std::endl;
+      sleep(1);
+      system("clear");
+
+      std::cout << "We will need some information from you. Please fill in the following form." << std::endl;
+      std::cin.ignore();
+
+      std::cout << "Your name: ";
+      std::getline(std::cin, name);
+
+      std::cout << "Your date of birth: ";
+      std::getline(std::cin, dob);
+
+      std::cout << "Your social ID: ";
+      std::getline(std::cin, socialID);
+
+      std::cout << "Your license ID: ";
+      std::getline(std::cin, licenseID);
+
+      std::cout << "We are almost finished. Now we just want to know if you are male or female" << std::endl
+                << "(1/0), defaults to male: ";
+      std::getline(std::cin, foo);
+      male = foo.compare(std::to_string(1)) == 0 ? true : false;
+
+      system("clear");
+      std::cout << "Well done. Choose a vehicle and we are ready to go." << std::endl;
+      for (int i = 0; i < vehicles.size(); i += 1)
+      {
+        if (vehicles[i]->isAvailable())
+          std::cout << "[" << i << "] " << vehicles[i]->getYear() << " " << vehicles[i]->getBrand() << " " << vehicles[i]->getModel() << " " << vehicles[i]->getType() << std::endl;
+      }
+      std::cout << "Your choice? ";
+      std::getline(std::cin, foo);
+      opt = stoi(foo);
+
+      system("clear");
+      if (BnR->newRent(name, socialID, licenseID, dob, male, vehicles[opt]) != 0)
+        std::cout << "Sorry. Something went wrong with the rental." << std::endl;
+      else
+        std::cout << "Your contract has been recorded." << std::endl;
+      break;
+
+    case '2':
+      std::cout << "Gotcha. Getting you there right now." << std::endl;
+      sleep(1);
+      system("clear");
+
+      std::cout << "We will need some information from you. Please fill in the following form." << std::endl;
+      std::cin.ignore();
+
+      std::cout << "Your name: ";
+      std::getline(std::cin, name);
+
+      std::cout << "Your date of birth: ";
+      std::getline(std::cin, dob);
+
+      std::cout << "Your social ID: ";
+      std::getline(std::cin, socialID);
+
+      std::cout << "Your license ID: ";
+      std::getline(std::cin, licenseID);
+
+      std::cout << "We are almost finished. Now we just want to know if you are male or female" << std::endl
+                << "(1/0), defaults to male: ";
+      std::getline(std::cin, foo);
+      male = foo.compare(std::to_string(1)) == 0 ? true : false;
+
+      system("clear");
+      std::cout << "Well done. Choose a vehicle and we are ready to go." << std::endl;
+      for (int i = 0; i < vehicles.size(); i += 1)
+      {
+        if (vehicles[i]->isAvailable())
+          std::cout << "[" << i << "] " << vehicles[i]->getYear() << " " << vehicles[i]->getBrand() << " " << vehicles[i]->getModel() << " " << vehicles[i]->getType() << std::endl;
+      }
+      std::cout << "Your choice? ";
+      std::getline(std::cin, foo);
+      opt = stoi(foo);
+
+      system("clear");
+      if (BnR->newBook(name, socialID, licenseID, dob, male, vehicles[opt]) != 0)
+        std::cout << "Sorry. Something went wrong with the booking." << std::endl;
+      else
+        std::cout << "Your contract has been recorded." << std::endl;
+      break;
+
+    case '3':
+      std::cout << "Gotcha. Getting you there right now." << std::endl;
+      sleep(1);
+      system("clear");
+
+      for (int i = 0; i < vehicles.size(); i += 1)
+      {
+        if (vehicles[i]->isAvailable())
+          std::cout << "[" << i << "] " << vehicles[i]->getYear() << " " << vehicles[i]->getBrand() << " " << vehicles[i]->getModel() << " " << vehicles[i]->getType() << std::endl;
+      }
+      getchar();
+      break;
+
+    default:
+      std::cout << "Sorry but we don't serve that kind. Pick something more reasonable." << std::endl;
+      sleep(1);
+      system("clear");
+      break;
+    }
+  }
+
+  delete BnR;
+
   return 0;
 }
